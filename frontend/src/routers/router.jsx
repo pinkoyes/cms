21961ee3cba0a9a13e70f693d1f0d83decd3b5d4
pinkoyes <dotpriyanshu@gmail.com>
2024-11-08 00:34:02 +0530

    create browser-router

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,10 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import { createBrowserRouter } from "react-router-dom"
+import App from "../App"
+import Home from "../pages/Home"
+import ErrorPage from "../pages/ErrorPage"
+import Student from "../pages/Student"
+import College from "../pages/College"
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "student",
+                element: <Student />
+            },
+            {
+                path: "college",
+                element: <College />
+            }
+        ]
     }
 ]);
 
