0b757c8d2b1cbb4ea8a47af2f7af178650b7bbcf
pinkoyes <dotpriyanshu@gmail.com>
2024-11-12 17:12:43 +0530

    add global middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 import express from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
 
 const app = express();
+app.use(express.json({ limit: "30kb" }))
+app.use(express.urlencoded({ extended: true, limit: "30kb" }))
+app.use(cors(corsOptions))
+app.use(cookieParser())
+
 
 export default app;
