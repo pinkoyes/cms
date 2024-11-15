611a88e3b122efb419615fda198df9bed558978a
pinkoyes <dotpriyanshu@gmail.com>
2024-11-15 13:13:33 +0530

    edit button

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from '../ui/Button'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>CMS</h1>
+            </div>
+            <div>
+                <h2>Services</h2>
+                <h2>About</h2>
+                <Button></Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
