1a024c8ee0946880065baf50baa95a49eac157ae
pinkoyes <dotpriyanshu@gmail.com>
2024-11-16 18:32:15 +0530

    update home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import AppInfo from '../components/AppInfo';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
-      <div className='mx-12 mt-6'>
+      <div className='px-12 mt-6'>
         <Navbar />
         <Hero />
+        <AppInfo />
+        <Footer />
       </div>
     </>
   );
