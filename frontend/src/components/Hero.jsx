b795a28f2c6a86ba124c52807df33cd5284403ff
pinkoyes <dotpriyanshu@gmail.com>
2024-11-17 00:08:46 +0530

    udate hero comp

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,10 +8,9 @@ const Hero = () => {
         <div className='w-1/2 text-center'>
             <h1 className='text-black font-bold text-4xl relative bottom-40'>Streamline Your College Management</h1>
             <p className='text-gray-600 text-lg font-light relative bottom-20'>Join Dotnext to effortlessly manage student attendance, results, fees, and more. Empower your college with our comprehensive management system today.</p>
-            <Button><Link to='/'>Get started</Link></Button>
+            <Button><Link to='/ask'>Get started</Link></Button>
         </div>
     </div>
-
   )
 }
 
